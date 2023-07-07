0732053531721dfc8fb12b57341695fd38a59210
youssefroulamellah <58234136+youssefroulamellah@users.noreply.github.com>
2023-07-07 02:16:33 -0400

    Added classes in main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,69 @@
 import time
 import logging
 import json
-import os
 from datetime import datetime
 import requests
 from signalrcore.hub_connection_builder import HubConnectionBuilder
-from src.db_manager import DBManager
+import warnings
+import os
+import pyodbc
 
 
 class UndefinedTokenValue(Exception):
     def __init__(self, default_value):
         message = f"A token value has not been defined, default value returned: {default_value}"
         super().__init__(message)
+
+
+class DBManager:
+    """
+    A class for managing the database connection and operations.
+    """
+    DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
+
+    def __init__(self):
+        """
+        Initializes a new instance of the DBManager class.
+        """
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        print("connection attempt")
+        self._conn = pyodbc.connect(self.DB_CONNECTION_STRING)
+        self._cursor = self._conn.cursor()
+        print("connection successful")
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.close()
+
+    @property
+    def connection(self):
+        """
+        Gets the database connection.
+        """
+        return self._conn
+
+    @property
+    def cursor(self):
+        """
+        Gets the database cursor.
+        """
+        return self._cursor
+
+    def commit(self):
+        """
+        Commits the current transaction.
+        """
+        self.connection.commit()
+
+    def close(self, commit=True):
+        """
+        Closes the database connection.
+        """
+        if commit:
+            self.commit()
+        self.connection.close()
 
 
 class Main:
